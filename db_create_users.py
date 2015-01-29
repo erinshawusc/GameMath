2a2114d329f4a9a6b584398d8aeeae2a76dd40d6
Akhil Bhiwal <bhiwalakhil@gmail.com>
2015-01-29 01:12:52 -0800

    updating readme

--- db_create_users.py
+++ db_create_users.py
@@ -2,9 +2,9 @@ from project import db
 from project.models import User
 
 # insert data
-db.session.add(User("michael", "michael@realpython.com", "i'll-never-tell"))
+db.session.add(User("erin", "shaw@isi.edu", "shaw"))
 db.session.add(User("admin", "ad@min.com", "admin"))
-db.session.add(User("mike", "mike@herman.com", "tell"))
+db.session.add(User("zehui", "zehuizha@usc.edu", "zhang"))
 
 # commit the changes
 db.session.commit()
